a52a980f4d51137419bc6275b21623b195279a0d
Garrett Chestnut <gchestnu@stevens.edu>
2021-05-04 15:35:58 -0400

    Implemented 'groups' db methods
    These methods haven't been implemented yet, but my updated 'movieSelection.js' demonstrates how they might be used.

--- routes/movieSelection.js
+++ routes/movieSelection.js
@@ -1,34 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const groups = require('./groups');
 
 router.use(express.static('public'));
-
-// represents list of movies held in 'currentSession.compiledList', would be replaced w/ db call
-groupMovies = ["Movie1", "Movie2", "Movie3", "Movie4", "Movie5", "Movie6", "Movie7", "Movie8"]
 
 router.get('/', async (req, res) => {
     res.render('movieSelection/home')
 });
 
 router.get('/list', async (req, res) => {
+    sesh = req.session.user
+    group = await groups.getGroup(sesh.groupID)
+
     // will keep track of how many movies the user has selected yay/nay on (compared to total # of movies)
-    req.session.user = { judged: 0, movie_count: groupMovies.length }
-    // this would be a database call, ofc
-    res.render('movieSelection/selection', { movies: groupMovies })
+    sesh.judged = 0
+    sesh.movie_count = group.groupMovieList.length
+    res.render('movieSelection/selection', { movies: group.groupMovieList })
 });
 
 router.post('/choice', async (req, res) => {
     decision = req.params.dec
     movie = req.params.mov
-    
-    // First check if currentSession.selection != null. If it doesn't then, a movie has been selected,
+    group = groups.getGroup(sesh.groupID)
+    grpSession = group.currentSession
+    // First check if grpSession.selection != null. If it doesn't then, a movie has been selected,
     // and the user should return to group home page where the chosen movie will be displayed
     sesh = req.session.user
     sesh.judged++;
     if(decision) {
-        // if 'yes' post response to map of movies -> 'currentSession.compiledList[movie]++'
-        // also check if value of 'currentSession.compiledList[movie]' equals 'currentSession.threshold'
-                    // if it is, set currentSession.selection equal to movie
+        // if 'yes' post response to map of movies -> 'grpSession.compiledList[movie]++'
+        // also check if value of 'grpSession.compiledList[movie]' equals 'grpSession.threshold'
+                    // if it is, set grpSession.selection equal to movie
     } else if(sesh.judged == sesh.movie_count) {
         // if user no longer has movies to judge, send them back to group home to
         // wait for the rest of group members to finish
