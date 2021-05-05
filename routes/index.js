9e82fbe6f2ec2737c19e63c932e2f9d18d73ca8c
Garrett Chestnut <gchestnu@stevens.edu>
2021-05-05 13:37:56 -0400

    Fleshed out movieSelection.js
    My code now assumes that 'movieSelection/home' will be the landing page for decision rooms.
    
    This is slightly different from before, but there needs to be a page/handlebars layout to send the user back to after the decision process is over, which should likely be the decision room home (will display final movie).

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 // import routes
 const wantToWatchListRoutes = require('./wantToWatchList');
+const movieSelectRoutes = require('./movieSelection')
 
 const constructorMethod = (app) => {
   // routes
 
   //route for building the want to watch list
   app.use("/wantToWatchList", wantToWatchListRoutes);
+
+  //routes for movie selection process
+  app.use("/movieSelection", movieSelectRoutes)
 
   app.use("/", (req, res) => {
     res.render("temporary/fakeLanding", { title: "Temporary Landing Page" });
--- routes/movieSelection.js
+++ routes/movieSelection.js
@@ -5,6 +5,8 @@ const groups = require('./groups');
 router.use(express.static('public'));
 
 router.get('/', async (req, res) => {
+    // This is the landing page for decision rooms
+    // Decision rooms != watch groups, so anyone(?) can join a decision room
     res.render('movieSelection/home')
 });
 
@@ -20,20 +22,29 @@ router.get('/list', async (req, res) => {
 
 router.post('/choice', async (req, res) => {
     decision = req.params.dec
-    movie = req.params.mov
+    movie = req.params.mov // movieID (from TMDb)
     group = groups.getGroup(sesh.groupID)
     grpSession = group.currentSession
     // First check if grpSession.selection != null. If it doesn't then, a movie has been selected,
     // and the user should return to group home page where the chosen movie will be displayed
+    if(grpSession.selection != "N/A") {
+        res.redirect('/')
+    }
     sesh = req.session.user
     sesh.judged++;
     if(decision) {
-        // if 'yes' post response to map of movies -> 'grpSession.compiledList[movie]++'
-        // also check if value of 'grpSession.compiledList[movie]' equals 'grpSession.threshold'
+        // check if value of 'grpSession.compiledList[movie]' equals 'grpSession.threshold'- 1
+        // (obviously, no need for additional DB call to update 'yes' count)
                     // if it is, set grpSession.selection equal to movie
+        if(grpSession.movie == grpSession.threshold - 1) {
+            // 'declareMovie() is a db call that sets currentSession.selection equal to 'movieID'
+            groups.declareMovie(sesh.groupID, movie)
+        }
+        groups.updateMovie(movie)
     } else if(sesh.judged == sesh.movie_count) {
         // if user no longer has movies to judge, send them back to group home to
         // wait for the rest of group members to finish
+        res.redirect('/')
     }
 });
 
