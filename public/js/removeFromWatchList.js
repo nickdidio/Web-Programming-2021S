e4022169a6326ebba54c9e67d71c8b7f26559cfd
nickdidio <nicholasdidio9@gmail.com>
2021-05-10 12:36:06 -0400

    Added some functionality for remove Watch List

--- public/js/removeFromWatchList.js
+++ public/js/removeFromWatchList.js
@@ -1,3 +1,33 @@
 (function ($) {
+    const moviesList = $("#movieList");
+    const errorMsg = $(".error");
 
+    const bindEventsToMovieItem = (movieItem) => {
+        movieItem.on("click",'.removeLink', (event) => {
+            event.preventDefault();
+            var requestConfig = {
+            method: 'POST',
+            url: '/wantToWatchList/remove',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                movie: movieItem
+            })
+            };
+            $.ajax(requestConfig).then((response) => {
+            if(response){
+                movieItem.find(".removeLink").html("This movie has been removed");
+            }else{
+                errorMsg.empty();
+                errorMsg.append(`Error: This movie has already been removed.`);
+                errorMsg.attr("hidden", false);
+            }
+            });
+        });
+    }
+
+    $(document).ready(function (){
+        moviesList
+        .children()
+        .each((index, element) => bindEventsToMovieItem($(element)));
+    });
 });
