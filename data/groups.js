e095803d109cfc3011f0e6438e5b1a9a8e22ad7d
ReillyFitz <rtfitz99@gmail.com>
2021-05-13 12:48:48 -0400

    Updated groups and past sessions
    groups now have a "current session" which stores information needed to present and pick a movie

--- data/groups.js
+++ data/groups.js
@@ -10,20 +10,27 @@ const createGroup = async(groupLeaderId) => {
     } catch(e) {
         throw new Error ("Could not create group: Invalid ID")
     }
-
+    
     const groupCollection = await groups();   
     let newGroup = {
         groupMembers: [parsedLeaderId], //includes group leader
-        groupMovieList: [],
+        currentSession: {
+            sessionDate = new Date().getTime(), //Time since epoch
+            sessionMembers: groupMembers,
+            voteCountNeeded: 1,
+            movieList: [],
+            filters: [],
+            votes: []
+        }, //TODO: check if i need to initialize this to something else, gonna just initalize it to a mostly empty object which can be modified later
         pastSessions: [],
         groupLeaderId: parsedLeaderId,
     };
 
     const insertInfo = await groupCollection.insertOne(newGroup);   
-    if (insertInfo.insertedCount === 0) throw new Error ('Failed to update database');
+    if (insertInfo.insertedCount === 0) throw new Error ('Failed to create ');
     const newId = insertInfo.insertedId;
     let stringId = newId.toString(); 
-    const group = await groupCollection.findOne({ _id: stringId});
+    const group = await getGroupById(stringId);
     return group;
 };
 
@@ -38,12 +45,12 @@ const addGroupMember = async (groupId, userId) => {
         throw new Error ("Could not create group: Invalid ID for user or group")
     }
     const groupCollection = await groups();
-    const oldGroup = await groupCollection.findOne({ _id: parsedGroupId});
+    const oldGroup = await getGroupById(parsedGroupId);
     let updatedGroup = {
-        groupMembers: oldGroup.groupMembers + parsedUserId,
-        groupMovieList: oldGroup.groupMovieList,
-        pastSessions: oldGroup.pastSession,
-        groupLeader: oldGroup.groupLeader,
+        groupMembers: oldGroup.groupMembers.push(parsedUserId), 
+        currentSession: oldGroup.currentSession,
+        pastSessions: oldGroup.pastSessions,
+        groupLeaderId: oldGroup.parsedLeaderId,
     }
 
     const updateInfo = await groupCollection.updateOne({_id: parsedGroupId}, {$set: updatedGroup});
@@ -53,51 +60,105 @@ const addGroupMember = async (groupId, userId) => {
 };
 
 //Only needs to be called before users begin to select for decision night. Gets movies from users and adds them together
-const updateMovieList = async (groupId) => {
+//TODO: finish implementation of movielist in curresnt session then delete this functon
+// const updateMovieList = async (groupId) => {
+//     let parsedGroupId;
+//     try {
+//         parsedGroupId = ObjectId(groupId);
+//     } catch(e) {
+//         throw new Error ("Could not create group: Invalid ID for group")
+//     }
+
+//     const groupCollection = await groups();
+//     const group = await groupCollection.findOne({ _id: parsedGroupId});
+//     let members = group.groupMembers;
+//     let newList = [];
+//     for (let i = 0; i < members; i++) {
+//         newList.concat(members[i].watchList) //check varialbe for user watchlist
+//     }
+
+//     // TODO Filter past sessions newList.filter(movie => !arr2.includes(movie));
+//     const updateInfo = await groupCollection.updateOne({_id: parsedGroupId}, {$set: {groupMovieList: newList}});
+//     if (updateInfo.modifiedCount === 0) throw new Error ('Could not update watchlist');
+
+//     return await groupCollection.findOne({ _id: parsedGroupId});
+// };
+
+
+const getGroupById = async (groupId) => {
     let parsedGroupId;
     try {
         parsedGroupId = ObjectId(groupId);
     } catch(e) {
-        throw new Error ("Could not create group: Invalid ID for group")
+        throw new Error ("Could not get group: Invalid ID for group")
     }
-
     const groupCollection = await groups();
-    const group = await groupCollection.findOne({ _id: parsedGroupId});
-    let members = group.groupMembers;
-    let newList = [];
-    for (let i = 0; i < members; i++) {
-        newList.concat(members[i].watchList) //check varialbe for user watchlist
-    }
-
-    // TODO Filter past sessions newList.filter(movie => !arr2.includes(movie));
-    const updateInfo = await groupCollection.updateOne({_id: parsedGroupId}, {$set: {groupMovieList: newList}});
-    if (updateInfo.modifiedCount === 0) throw new Error ('Could not update watchlist');
-
-    return await groupCollection.findOne({ _id: parsedGroupId});
+    return await groupCollection.findOne({ _id: parsedGroupId});;
 };
 
-const addPastSession = async (groupId, session) => {
+const deleteGroup = async (groupId) => {
     let parsedGroupId;
     try {
         parsedGroupId = ObjectId(groupId);
     } catch(e) {
-        throw new Error ("Could not create group: Invalid ID for group")
+        throw new Error ("Could not delete group: Invalid ID for group")
     }
-
     const groupCollection = await groups();
-    const oldGroup = await groupCollection.findOne({ _id: parsedGroupId});
-    let updatedGroup = {
-        groupMembers: oldGroup.groupMembers,
-        groupMovieList: oldGroup.groupMovieList,
-        pastSessions: oldGroup.pastSession + session,
-        groupLeader: oldGroup.groupLeader,
-    }
 
-    const updateInfo = await groupCollection.updateOne({_id: parsedGroupId}, {$set: updatedGroup});
-    if (updateInfo.modifiedCount === 0) throw new Error ('Could not add past Session to group history');
-
-    return await groupCollection.findOne({ _id: parsedGroupId});
-
+    const deleted = await groupCollection.findOneAndDelete({_id: parsedGroupId,});
+      if (deleted.value === null) {
+        throw new Error(`Could not delete movie with id of ${id}`);
+      }
+      return { groupId: parsedGroupId, deleted: true };
 };
 
-module.exports = {createGroup, addGroupMember, updateMovieList, addPastSession};
+//Past Session will contain only important information about previous sessions, making it smaller than currentSession
+//Separating Current Session from Past Session allows us to cut down on unnesecary data which can be discarded after the session is finished.
+const createSession = async(groupId, voteCountNeeded, filters) => {
+    let parsedGroupId;
+    try {
+        parsedGroupId = ObjectId(groupId);
+    } catch(e) {
+        throw new Error ("Could not create session: Invalid group ID")
+    }
+    const groupCollection = await groups();   
+    const userCollection = await users();
+    let decisionGroup = getGroupById(parsedGroupId);
+    let sessionMembers = decisionGroup.groupMembers;
+    let movieList = [];
+
+    //Get combination of all users lists
+    for (let userId of sessionMembers) {
+        let parsedId = ObjectId(userId);
+        let user =  await userCollection.findOne({ _id: parsedId}); //replace with getUserById probbly for easier error handling
+        let userMovies = user.userMovieList; 
+        movieList += userMovies;
+    }
+
+    //TODO: Apply filters
+    for (let movie of movieList) {
+        //Genre
+        //Runtime
+        //MPAA Rating
+        //Previously watched
+    }
+    
+    //TODO: FILTER past sessions films
+    let newSession = {
+        sessionDate = new Date().getTime(), //Time since epoch
+        sessionMembers: groupMembers,
+        voteCountNeeded,
+        movieList: [],
+        filters,
+        votes: new Array(groupMembers.length).fill(0) //Array where nth item on array corresponds to vote for nth item in moveList
+    };
+
+    const updateInfo = await groupCollection.updateOne({_id: parsedGroupId}, {$set: {currentSession: newSession}});
+    if (updateInfo.modifiedCount === 0) throw new Error ('Could not create new session');
+
+    return await groupCollection.getGroupById(parsedGroupId);
+};
+
+
+
+module.exports = {createGroup, addGroupMember, getGroupById, deleteGroup, createSession/*, addVote, checkWinner*/};
--- data/pastSessions.js
+++ data/pastSessions.js
@@ -1,28 +1,48 @@
 const mongoCollections = require("../config/mongoCollections.js");
 const { ObjectId } = require("mongodb");
+const { pastSessions } = require("../config/mongoCollections.js");
+const pastSessions = mongoCollections.pastSessions;
 const groups = mongoCollections.groups;
 
-const createPastSession = async(sessionDate, sessionMembers, moviePicked) => {
+//Past Session will contain only important information about previous sessions, making it smaller than currentSession
+//Separating Current Session from Past Session allows us to cut down on unnesecary data which can be discarded after the session is finished.
+//Takes in groupId, {currentSession}, moviePicked 
+const createPastSession = async(groupId, {sessionDate, sessionMembers, voteCountNeeded, movieList, filters, votes}, moviePicked) => {
     let parsedMovieId;
+    let parsedGroupId
     try {
         parsedMovieId = ObjectId(moviePicked);
+        parsedGroupId = ObjectId(groupId);
     } catch(e) {
         throw new Error ("Could not create group: Invalid ID")
     }
 
-    const groupCollection = await groups();   
+    const psCollection = await pastSessions();   
     let newSession = {
+        parsedGroupId,
         sessionDate,
         sessionMembers,
         moviePicked: parsedMovieId
     };
 
-    const insertInfo = await groupCollection.insertOne(newSession);   
+    const insertInfo = await psCollection.insertOne(newSession);   
     if (insertInfo.insertedCount === 0) throw new Error ('Failed to update past session database');
-    const newId = insertInfo.insertedId;
-    let stringId = newId.toString(); 
-    const session = await groupCollection.findOne({ _id: stringId});
-    return session;
+
+    const groupCollection = await groups();
+    const group = await groupCollection.findOne({ _id: parsedGroupId});
+    group.pastSessions.push(newSession);
+    const updateInfo = await groupCollection.updateOne({_id: parsedId}, {$set: group});
+    if (updateInfo.modifiedCount === 0) throw 'Could not add past session';
 };
 
-module.exports = {createPastSession};
+const getPastSessionById = async(sessionId) => {
+    let parsedSessionId;
+    try {
+        parsedSessionId = ObjectId(sessionId);
+    } catch(e) {
+        throw new Error ("Could not get group: Invalid ID for group")
+    }
+    const psCollection = await pastSessions();
+    return await psCollection.findOne({ _id: parsedSessionId});;
+}
+module.exports = {createPastSession, getPastSessionById};
