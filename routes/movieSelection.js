5f80cdada7afb68c69a60ec1173b97140db5e040
Garrett Chestnut <gchestnu@stevens.edu>
2021-05-15 09:51:14 -0400

    Updated movieSelection.js

--- routes/movieSelection.js
+++ routes/movieSelection.js
@@ -7,94 +7,15 @@ const { movies } = require('../config/mongoCollections');
 const session = require('express-session');
 router.use(express.static('public'));
 
-let movies_test = [
-    {
-        "_id": "1",
-        "title": "Cats 1",
-        "desc": "A bunch of cats singing 1",
-        "img": "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop",
-    "reviews":[{
-        "_id": "8c7997a2-c0d2-4g8c-b27a-6c1d4b5b6310",
-        "reviewDate": "04/02/2021",
-        "reviewText": "PURRFECT. I loved Sir Patrick Stewart as the poop emoji!",
-        "username": "man1",
-        "rating": 5,
-        "movieId": "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310"
-          }],    
-          "releaseYear": 2019,
-        "runtime": 110,
-        "mpaaRating": "PG",
-        "userAvgRating": 1.1,
-        "genre": ["Musical", "Horror", "Western", "Thriller"]
-    },
-    {
-        "_id": "2",
-        "title": "Cats 2",
-        "desc": "A bunch of cats singing 2",
-        "img": "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop",
-    "reviews":[{
-        "_id": "8c7997a2-c0d2-4g8c-b27a-6c1d4b5b6310",
-        "reviewDate": "04/02/2021",
-        "reviewText": "MeOOOOwww Cats is great!!! Absolutely PURRFECT. I loved Sir Patrick Stewart as the poop emoji!",
-        "username": "man2",
-        "rating": 5,
-        "movieId": "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310"
-          }],    
-          "releaseYear": 2019,
-        "runtime": 110,
-        "mpaaRating": "PG",
-        "userAvgRating": 1.1,
-        "genre": ["Musical", "Horror", "Western", "Thriller"]
-    },
-    {
-        "_id": "3",
-        "title": "Cats 3",
-        "desc": "A bunch of cats singing 3",
-        "img": "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop",
-    "reviews":[{
-        "_id": "8c7997a2-c0d2-4g8c-b27a-6c1d4b5b6310",
-        "reviewDate": "04/02/2021",
-        "reviewText": "MeOOOOwww Cats is great!!! Absolutely PURRFECT. I loved Sir Patrick Stewart as the poop emoji!",
-         "username": "man3",
-        "rating": 5,
-        "movieId": "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310"
-          }],    
-          "releaseYear": 2019,
-        "runtime": 110,
-        "mpaaRating": "PG",
-        "userAvgRating": 1.1,
-        "genre": ["Musical", "Horror", "Western", "Thriller"]
-    },
-    {
-        "_id": "4",
-        "title": "Cats 4",
-        "desc": "A bunch of cats singing 4",
-        "img": "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop",
-    "reviews":[{
-        "_id": "8c7997a2-c0d2-4g8c-b27a-6c1d4b5b6310",
-        "reviewDate": "04/02/2021",
-        "reviewText": "MeOOOOwww Cats is great!!! Absolutely PURRFECT. I loved Sir Patrick Stewart as the poop emoji!",
-        "username": "man4",
-        "rating": 5,
-        "movieId": "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b6310"
-          }],    
-          "releaseYear": 2019,
-        "runtime": 110,
-        "mpaaRating": "PG",
-        "userAvgRating": 1.1,
-        "genre": ["Musical", "Horror", "Western", "Thriller"]
-    }
-]
-
 // sesh.active -> once a user enters starts judging, they're 'active' until a movie is selected as a winner by the group
 
 router.get('/', async (req, res) => {
-    /*if(!req.session.user) {
+    if(!req.session.user) {
         res.status(400).send("You must be logged in to access this page!")
     } else if(sesh.leader && !sesh.chosen) {
         // group leader shouldn't use this route
         return
-    }*/
+    }
     sesh = req.session
     sesh.groupID = req.query.id
     group = await groups.getGroupById(req.query.id)
@@ -104,8 +25,8 @@ router.get('/', async (req, res) => {
     }
     // fresh join (after leaving, or new session member)
     if(!sesh.chosen && !sesh.active) {
-        // applies to users who join session after the leader sets currentSession.active = true
-        if(group.currentSession.sessionMembers.includes("1") && group.currentSession.active) {
+        if(group.currentSession.sessionMembers.includes(sesh.user._id) && group.currentSession.active) {
+            //sesh.active = true
             res.redirect("/pick/list")
             return
         }
@@ -138,8 +59,7 @@ router.get('/', async (req, res) => {
         })
     } else {
         // return (movie chosen)
-        //movie_info = await movie.getMovieById(group.currentSession.chosen)
-        movie_info = group.currentSession.chosen
+        movie_info = await movie.getMovieById(group.currentSession.chosen)
         /*res.render('movieSelection/home', 
         { 
             title: `Chosen movie: ${movie_info.title}`,
@@ -159,25 +79,23 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/done', async(req, res) => {
-    /*if(!req.session.user) {
+    if(!req.session.user) {
         res.status(403).send("You must be logged in to access this page!")
-    }*/
+    }
     group = await groups.getGroupById(req.session.groupID)
-    //console.log(group)
     if(group.currentSession.chosen != "na") {
         sesh.chosen = true
         sesh.active = false
         res.redirect("/pick")
         return
     } else {
-        console.log("Not done!")
         res.render('movieSelection/home', 
         { 
             title: "Waiting on group members to pick movies.",
             exit: "appear",
             pick: "gone",
             done: "appear",
-            error: "Chill out, your group members aren't ready yet!"
+            error: "Chill out, your group members aren't ready yet."
         })
     }
 })
@@ -195,32 +113,31 @@ router.get('/leave', async (req, res) => {
 })
 
 router.get('/list', async (req, res) => {
-    /*if(!req.session.user) {
+    if(!req.session.user) {
         res.status(403).send("You must be logged in to access this page!")
-    }*/
+    }
     sesh = req.session
     group = await groups.getGroupById(sesh.groupID)
     // if user clicked "Pick Flicks" button too early, send them back
     if(!group.currentSession.active) {
-        // should present error message, and return to original page instead
-        res.status(400).end()
+        // should also present error message
+        res.status(400)
         return
     }
     sesh.active = true
     // will keep track of how many movies the user has selected yay/nay on (compared to total # of movies)
     sesh.judged = 0
     // these ids would be converted into movie objects
-    sesh.movie_list = movies_test
-    /*for(item of group.currentSession.movieList) {
+    sesh.movie_list = []
+    for(item of group.currentSession.movieList) {
         sesh.movie_list.push(item.movie)
-    }*/
+    }
     /*for (const [key, value] of Object.entries(group.currentSession.roster)) {
         sesh.movie_list.push(value)
     }*/
     sesh.movie_count = sesh.movie_list.length
     
-    //movie = await movies.getMovieById(sesh.movie_list[0])
-    movie = sesh.movie_list[0]
+    movie = await movies.getMovieById(sesh.movie_list[0])
     res.render('movieSelection/selection', 
     { 
         movie: movie,
@@ -230,13 +147,13 @@ router.get('/list', async (req, res) => {
 });
 
 router.post('/choice/:dec', async (req, res) => {
-    /*if(!req.session.user) {
+    if(!req.session.user) {
         res.status(403).send("You must be logged in to access this page!")
         return
     } else if(!req.query[dec]) {
         res.status(400).send("Must provide a judgement! (yes/no).")
         return
-    }*/
+    }
     decision = req.params.dec
     sesh = req.session
     movie = sesh.movie_list[sesh.judged] // movieID (from TMDb)
@@ -253,17 +170,11 @@ router.post('/choice/:dec', async (req, res) => {
 
     sesh.judged++;
     if(decision == "yes") {
-        console.log("mS: " + sesh.groupID + " " + movie._id)
-        result = await groups.addVote(sesh.groupID, movie._id)
-        console.log(result)
+        result = await groups.addVote(sesh.groupID, movie)
         if(result.winner) {
             //  groups.declareMovie(sesh.groupID, movie) ^^ Could be done by "addVote" function\
             sesh.chosen = true
             sesh.active = false
-            res.redirect('/pick')
-            return
-        } else if(sesh.judged == sesh.movie_count) {
-            sesh.active = true
             res.redirect('/pick')
             return
         }
@@ -275,10 +186,8 @@ router.post('/choice/:dec', async (req, res) => {
         res.redirect('/pick')
         return
     }
-    console.log(sesh.judged + " " + sesh.movie_count)
     // get next movie based on user session progress
-    //next_movie = await movies.getMovieById(sesh.movie_list[sesh.judged])
-    next_movie = sesh.movie_list[sesh.judged]
+    next_movie = await movies.getMovieById(sesh.movie_list[sesh.judged])
     //console.log(grpSession)
     res.render('movieSelection/selection', 
     { 
